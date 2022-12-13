7f9c5b2992a557a120726ebb4a6453cdf7bc298c
rwiankowski <rwiankowski@mac.com>
2022-12-13 22:09:59 +0100

    After stage 3

--- Git Internals/task/src/gitinternals/Main.kt
+++ Git Internals/task/src/gitinternals/Main.kt
@@ -1,6 +1,8 @@
 package gitinternals
 
 import java.io.FileInputStream
+import java.time.Instant
+import java.time.ZoneId
 import java.util.zip.InflaterInputStream
 
 fun main() {
@@ -17,8 +19,66 @@ fun main() {
     var inflatedInput = ""
 
     inflaterInputStream.readAllBytes().map {
-        inflatedInput += it.toChar()
+        inflatedInput += it.toInt().toChar()
     }
-    val firstLine = inflatedInput.split('\u0000')[0].split(' ')
-    println("type:${firstLine[0]} length:${firstLine[1]}")
+
+    //println(inflatedInput)
+
+    val inflatedInputSplit = inflatedInput.split('\u0000')
+    val objectType = inflatedInputSplit[0].split(' ')[0]
+    var output = "*${objectType.uppercase()}*"
+
+    when(objectType) {
+        "commit" -> {
+            val lines = inflatedInputSplit[1].split("\n")
+            lines.forEach { line ->
+                when {
+                    (line.matches(("""tree .*""").toRegex())) -> {
+                        val body = line.split(' ')[1]
+                        output += "\ntree: $body"
+                    }
+
+                    (line.matches(("""parent .*""").toRegex())) -> {
+                        val body = line.split(' ')[1]
+                        output += if (output.contains("parents:")) " | $body"
+                        else "\nparents: $body"
+                    }
+
+                    (line.matches(("""author .*""").toRegex())) -> {
+                        val body = line.split(' ')[1]
+                        val contact = line.split(' ')[2]
+                        val contactClean = contact.substring(1, contact.length - 1)
+                        val epochSecond = line.split(' ')[3]
+                        val zoneId = ZoneId.of(line.split(' ')[4])
+                        val instant = Instant.ofEpochSecond(epochSecond.toLong()).atZone(zoneId)
+                        output += "\nauthor: $body $contactClean original timestamp: ${instant.toLocalDate()} ${instant.toLocalTime()} ${instant.zone}"
+                    }
+
+                    (line.matches(("""committer .*""").toRegex())) -> {
+                        val body = line.split(' ')[1]
+                        val contact = line.split(' ')[2]
+                        val contactClean = contact.substring(1, contact.length - 1)
+                        val epochSecond = line.split(' ')[3]
+                        val zoneId = ZoneId.of(line.split(' ')[4])
+                        val instant = Instant.ofEpochSecond(epochSecond.toLong()).atZone(zoneId)
+                        output += "\ncommitter: $body $contactClean commit timestamp: ${instant.toLocalDate()} ${instant.toLocalTime()} ${instant.zone}"
+                    }
+
+                    (line.matches(("""""").toRegex())) -> {
+                        if(!output.contains("commit message:")) output += "\ncommit message:"
+                    }
+
+                    else -> {
+                        output += "\n$line"
+                    }
+                }
+            }
+        }
+        "blob" -> {
+            output += "\n${inflatedInputSplit[1]}"
+        }
+        else -> return
+    }
+
+    println(output)
 }
